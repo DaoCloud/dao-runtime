d3567e0d778a80eb91cbb774258443f501f3c3e4
pyu <p.yu@daocloud.io>
2015-02-10 23:57:04 -0800

    Implement register and unregister runtime
    Also rename web.server to web.api to avoid conflicting with server.
    
    Samples:
    
    $ curl -i http://localhost:5000/api/v1/runtimes -d '{"name":"sample"}'
    -H Content-Type:application/json
    HTTP/1.0 200 OK
    Content-Type: text/html; charset=utf-8
    Content-Length: 0
    Server: Werkzeug/0.10.1 Python/2.7.8
    Date: Wed, 11 Feb 2015 07:58:19 GMT
    
    $ curl -i http://localhost:5000/api/v1/runtimes/sample -X DELETE
    HTTP/1.0 200 OK
    Content-Type: text/html; charset=utf-8
    Content-Length: 0
    Server: Werkzeug/0.10.1 Python/2.7.8
    Date: Wed, 11 Feb 2015 07:58:35 GMT

--- server/server/web/v1.py
+++ server/server/web/v1.py
@@ -1,16 +1,26 @@
-from server import api, api_url, Service
+from flask import request, make_response
+
+from api import api, api_url, Service
+from server.registry import register, unregister, Runtime
 
 VERSION = "v1"
 
 
 @api.route(api_url(VERSION, Service.REGISTER), methods=['POST'])
-def register():
-    return ''
+def register_runtime():
+    payload = request.get_json()
+    if not payload or 'name' not in payload:
+        return make_response('Bad Request', 400)
+    else:
+        runtime = payload['name']
+        register(Runtime(runtime))
+        return ''
 
 
 @api.route(api_url(VERSION, Service.UNREGISTER), methods=['DELETE'])
-def unregister(runtime):
-    return 'unregister ' + runtime
+def unregister_runtime(runtime):
+    unregister(runtime)
+    return ''
 
 
 @api.route(api_url(VERSION, Service.POLLING), methods=['GET'])
--- server/tests/web/test_v1.py
+++ server/tests/web/test_v1.py
@@ -1,25 +1,49 @@
 
-import unittest
-from server.web.server import api
-import server.web.v1  # noqa
-
 from hamcrest import *  # noqa
+import unittest
+
+from server.web.api import api
+import server.web.v1  # noqa
+import server.registry as registry
 
 
 class TestApiV1(unittest.TestCase):
 
     def setUp(self):
         self.app = api.test_client()
+        registry.clear()
 
-    def test_register(self):
-        result = self.app.post('/api/v1/runtimes')
+    def test_register_unregister(self):
+        result = self.app.post('/api/v1/runtimes',
+                               headers={'Content-Type': 'application/json'},
+                               data='{"name":"sample"}')
         assert_that(result.status_code, is_(200))
         assert_that(result.data, is_(''))
+        assert_that(registry.size(), is_(1))
+        assert_that(registry.is_registered('sample'), is_(True))
 
-    def test_unregister(self):
         result = self.app.delete('/api/v1/runtimes/sample')
         assert_that(result.status_code, is_(200))
-        assert_that(result.data, is_('unregister sample'))
+        assert_that(registry.size(), is_(0))
+        assert_that(registry.is_registered('sample'), is_(False))
+
+    def test_invalid_register(self):
+        # Body not json
+        result = self.app.post('/api/v1/runtimes',
+                               headers={'Content-Type': 'application/json'},
+                               data='not_json')
+        assert_that(result.status_code, is_(400))
+
+        # No Content-Type
+        result = self.app.post('/api/v1/runtimes',
+                               data='not_json')
+        assert_that(result.status_code, is_(400))
+
+        # Missing field
+        result = self.app.post('/api/v1/runtimes',
+                               headers={'Content-Type': 'application/json'},
+                               data='{"l_name": "Turing","f_name": "Alan"}')
+        assert_that(result.status_code, is_(400))
 
     def test_polling(self):
         result = self.app.get('/api/v1/runtimes/sample/requests')
